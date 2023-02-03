c1cfd08a8d02403dfc0f8a1a42956980768accdf
Magdalena <magdalena.a.broniszewska@gmail.com>
2023-02-03 16:23:11 +0100

    added crew mobile

--- script.js
+++ script.js
@@ -10,56 +10,70 @@ const destinationPlanetImg = document.querySelector(".destination__planet-img");
 const destinationDistanceNum = document.querySelector(".distance-number");
 const destinationTravelNum = document.querySelector(".travel-number");
 
+/////NAV///////
 btn.addEventListener("click", function () {
 	navMenu.classList.toggle("active");
 	navMenu.classList.contains("active")
-		? (btn.style.backgroundImage =
-				"url(assets/shared/icon-close.svg)")
-		: (btn.style.backgroundImage =
-				"url(assets/shared/icon-hamburger.svg)");
+		? (btn.style.backgroundImage = "url(assets/shared/icon-close.svg)")
+		: (btn.style.backgroundImage = "url(assets/shared/icon-hamburger.svg)");
 });
 
 /// Get JSON DATA
 const getData = async function () {
 	const res = await fetch("./data.json");
 	const data = await res.json();
-	console.log(data);
 	return data;
 };
 
 /// RENDER DATA
 
-const renderPlanetData = function (planet) {
-	destinationTitle.textContent = planet.name;
-	destinationText.textContent = planet.description;
-	destinationPlanetImg.src = planet.images.png;
-	destinationDistanceNum.textContent = planet.distance;
-	destinationTravelNum.textContent = planet.travel;
+const renderPlanetData = function (planet, i) {
+	destinationTitle.textContent = planet[i].name;
+	destinationText.textContent = planet[i].description;
+	destinationPlanetImg.src = planet[i].images.png;
+	destinationDistanceNum.textContent = planet[i].distance;
+	destinationTravelNum.textContent = planet[i].travel;
 };
 
 //// CHANGING PLANET DATA IN DOM
 planetsArr.forEach((planet, i) => {
 	planet.addEventListener("click", function (e) {
 		e.preventDefault();
-		planetsArr.forEach(planet => {
-			planet.classList.remove("border");
-		});
-		e.target.classList.add("border");
-
+		planetsArr.forEach(planet => planet.classList.remove("border"));
+		planet.classList.add("border");
 		const dataJson = getData().then(data => {
 			const { destinations } = data;
-			const moon = destinations[0];
-			const mars = destinations[1];
-			const europa = destinations[2];
-			const titan = destinations[3];
+			renderPlanetData(destinations, i);
+		});
+	});
+});
 
-			i === 0
-				? renderPlanetData(moon)
-				: i === 1
-				? renderPlanetData(mars)
-				: i === 2
-				? renderPlanetData(europa)
-				: renderPlanetData(titan);
+////////CREW//////
+
+const crewImg = document.querySelector(".crew__img");
+const crewProfession = document.querySelector(".crew__profession");
+const crewMemberName = document.querySelector(".crew__name");
+const crewMemberDescription = document.querySelector(".crew__description");
+const sliderBtn = document.querySelectorAll(".slider__btn");
+const slidersBtnArr = Array.from(sliderBtn);
+
+const renderCrewData = function (data, i) {
+	crewImg.src = data[i].images.png;
+	crewProfession.textContent = data[i].role;
+	crewMemberName.textContent = data[i].name;
+	crewMemberDescription.textContent = data[i].bio;
+};
+//// CHANGING CREW DATA IN DOM
+slidersBtnArr.forEach((slider, i) => {
+	slider.addEventListener("click", function (e) {
+		e.preventDefault();
+		slidersBtnArr.forEach(slider =>
+			slider.classList.remove("slider__btn--active")
+		);
+		slider.classList.add("slider__btn--active");
+		const dataJson = getData().then(data => {
+			const { crew } = data;
+			renderCrewData(crew, i);
 		});
 	});
 });
